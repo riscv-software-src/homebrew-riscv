02156671a3bc5a6714b7ed6164551b2d9d780911
Scott Beamer <sbeamer@eecs.berkeley.edu>
2014-08-22 17:13:23 -0700

    add test for riscv-tools and remove comments for rest

--- riscv-pk.rb
+++ riscv-pk.rb
@@ -24,15 +24,6 @@ class RiscvPk < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test riscv`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
--- riscv-tools.rb
+++ riscv-tools.rb
@@ -13,15 +13,12 @@ class RiscvTools < Formula
   depends_on "riscv-pk"
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test riscv`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    (testpath/'hello.c').write('#include<stdio.h>
+ int main() {
+   printf("hi\n");
+   return 0;
+ }')
+    system "#{HOMEBREW_PREFIX}/bin/riscv-gcc", (testpath/'hello.c')
+    system "#{HOMEBREW_PREFIX}/bin/spike", "#{HOMEBREW_PREFIX}/bin/pk", (testpath/'a.out')
   end
 end
