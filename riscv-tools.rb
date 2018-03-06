5b627e00cec175a66d5e05db5ebc791793125da2
Daniel M. Zimmerman <dmz@galois.com>
2018-03-05 17:03:53 -0800

    Updated other places where "riscv-gcc" should change to "riscv-gnu-toolchain".

--- riscv-tools.rb
+++ riscv-tools.rb
@@ -4,12 +4,12 @@ class RiscvTools < Formula
   homepage "http://riscv.org"
   url "https://github.com/riscv/riscv-tools/archive/homebrew.tar.gz"
   sha256 "cb919eb7cf11071c6d11c721a9e77893a2dbe9158466e444eb3dd8476a89b7b4"
-  version "0.1"
+  version "0.2"
 
   # install rest of tools
   depends_on "riscv-fesvr"
   depends_on "riscv-isa-sim"
-  depends_on "riscv-gcc"
+  depends_on "riscv-gnu-toolchain"
   depends_on "riscv-pk"
 
   def install
