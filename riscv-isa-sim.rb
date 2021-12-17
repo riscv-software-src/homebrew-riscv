7d2a328a77c0251236303cf4b7b4c3e2f88f3452
Kamyar Mohajerani <kammoh@gmail.com>
2021-12-17 01:35:14 -0500

    build riscv-isa-sim with boost [RFC] (#71)
    * fix build when boost is detected
    
    require boost anyways
    
    * disable boost by default instead

--- riscv-isa-sim.rb
+++ riscv-isa-sim.rb
@@ -11,13 +11,27 @@ class RiscvIsaSim < Formula
   end
 
   depends_on "dtc"
+  depends_on "boost" => :optional
+
+  opoo "Building with clang could fail due to upstream bug: https://github.com/riscv-software-src/riscv-isa-sim/issues/820" if build.with? "boost"
 
   def install
     mkdir "build"
     cd "build" do
-      # configure uses --with-target to set TARGET_ARCH
-      system "../configure", "--prefix=#{prefix}", "--with-target=\"\""
-      system "make", "prefix=#{HOMEBREW_PREFIX}"
+      args = [
+        "--prefix=#{prefix}"
+      ]
+      if build.with? "boost"
+        # This seems to be needed at least on macos/arm64
+        args << "--with-boost=#{Formula["boost"].prefix}"
+      else
+        args << "--without-boost"
+        args << "--without-boost-asio"
+        args << "--without-boost-regex"
+      end
+      # configure uses --with-target to set TARGET_ARCH but homebrew formulas only provide "with"/"without" options
+      system "../configure", *args 
+      system "make"
       system "make", "install"
     end
   end
